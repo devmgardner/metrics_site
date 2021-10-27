86e704a85adbf9b36c8867a4c83954c440467eb3
Dev M <57118036+devmgardner@users.noreply.github.com>
2021-10-27 12:45:37 -0400

    switching devices

--- ergosite/scrape.py
+++ ergosite/scrape.py
@@ -1,8 +1,8 @@
 import xml.etree.ElementTree as ET, sqlite3 as sq, re, requests as rq
 
 
-ergodb = sq.connect('ergo.sqlite')
-ergocon = ergodb.cursor()
+ergocon = sq.connect('ergo.sqlite')
+ergocur = ergocon.cursor()
 
 
 url = 'https://secure.runescape.com/m=adventurers-log/rssfeed?searchName=Ergo'
@@ -15,3 +15,4 @@ for item in feed:
     link = item.find('link').text
     date = item.find('pubDate').text
     itemid = re.match('.*[&id=-](?P<id>\d*)',item.find('link').text).group('id')
+    ergocur.execute('INSERT INTO Events (id, title, description, link, date) VALUES (itemid, title, description, link, date)')
