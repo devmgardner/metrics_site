2d66fdca69fec334f1ae9f0a8b033875b0684fd5
devmgardner <devm.gardner@gmail.com>
2021-10-28 10:56:32 -0400

    info on runemetrics API

--- ergosite/scrape.py
+++ ergosite/scrape.py
@@ -5,50 +5,49 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 #
-fname = f'{currentdir}/alintharscrape-log.txt'
-fhand = open(fname, 'w')
-fhand.close()
-fhand = open(fname, 'r+')
-
-
-ergocon = sq.connect(f'{currentdir}/alinthar.sqlite')
+fname = f'{currentdir}\ergoscrape-log.txt'
+try:
+    fhand = open(fname, 'a')
+except FileNotFoundError:
+    fhand = open(fname, 'w')
+ergocon = sq.connect(f'{currentdir}\ergo.sqlite')
 ergocur = ergocon.cursor()
 
 
-url = 'https://secure.runescape.com/m=adventurers-log/rssfeed?searchName=Alinthar'
+url = 'https://secure.runescape.com/m=adventurers-log/rssfeed?searchName=Ergo'
 response = rq.get(url)
-date = time.asctime(time.gmtime())
+#date = time.asctime(time.gmtime())
 data = ET.fromstring(response.text)
 feed = data[0].findall('item')
 for item in feed:
     try:
         title = item.find('title').text
     except:
-        fhand.write(f'Error finding item title at time: {time.asctime(time.gmtime())}.\n')
+        fhand.write(f'{time.strftime("%m-%d-%Y %H:%M:%S",time.gmtime())} UTC - nferr title\n')
         continue
     try:
         description = re.sub('\t','',re.sub('\n','',item.find('description').text))
     except:
-        fhand.write(f'Error finding item description at time: {time.asctime(time.gmtime())}.\n')
+        fhand.write(f'{time.strftime("%m-%d-%Y %H:%M:%S",time.gmtime())} UTC - nferr description\n')
         continue
     try:
         link = item.find('link').text
     except:
-        fhand.write(f'Error finding item link at time: {time.asctime(time.gmtime())}.\n')
+        fhand.write(f'{time.strftime("%m-%d-%Y %H:%M:%S",time.gmtime())} UTC - nferr link\n')
         continue
-    #try:
-    #    date = item.find('pubDate').text
-    #except:
-    #    fhand.write(f'Error finding item date at time: {time.asctime(time.gmtime())}.\n')
-    #    continue
+    try:
+        date = item.find('pubDate').text
+    except:
+        fhand.write(f'{time.strftime("%m-%d-%Y %H:%M:%S",time.gmtime())} UTC - nferr date\n')
+        continue
     try:
         itemid = re.match('.*[&id=-](?P<id>\d*)',item.find('link').text).group('id')
     except:
-        fhand.write(f'Error finding item id at time: {time.asctime(time.gmtime())}.\n')
+        fhand.write(f'{time.strftime("%m-%d-%Y %H:%M:%S",time.gmtime())} UTC - nferr itemid\n')
         continue
     if len(ergocur.execute('SELECT * FROM Events WHERE itemid = ?', (itemid,)).fetchall()) == 0:
         ergocur.execute('INSERT INTO Events (itemid, title, description, link, date) VALUES (?, ?, ?, ?, ?)', (itemid, title, description, link, date,))
         ergocon.commit()
     else:
-        fhand.write(f'Item {itemid} already exists in database, skipping.\n')
+        fhand.write(f'{time.strftime("%m-%d-%Y %H:%M:%S",time.gmtime())} UTC - {itemid} dupcheck\n')
         continue
