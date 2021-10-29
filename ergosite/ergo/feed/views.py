90b0ff9f654d6dae970bfa5a1e6b5034451ab174
Dev M <57118036+devmgardner@users.noreply.github.com>
2021-10-28 20:16:10 -0400

    building from iportfolio template

--- ergosite/ergo/feed/views.py
+++ ergosite/ergo/feed/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def feed(request) :
-    return render(request, 'feed/feed.html')
+    return render(request, 'feed/iportfolio.html')
