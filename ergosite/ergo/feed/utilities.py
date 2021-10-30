18166978c2bdf8facac2d08020e85b8dcb7d7ea3
Dev M <57118036+devmgardner@users.noreply.github.com>
2021-10-29 21:26:15 -0400

    layout mostly finished

--- ergosite/ergo/feed/utilities.py
+++ ergosite/ergo/feed/utilities.py
@@ -63,4 +63,4 @@ def apiscrape(u):
     data = rq.get(url).json()
     #with open(f'{currentdir}\{u}.txt','w') as fhand:
     #    json.dump(data, fhand, indent=2)
-    return [data['combatlevel'], data['totalskill'], data['totalxp'], data['rank'], data['skillvalues']]
+    return [data['combatlevel'], data['totalskill'], data['totalxp'], "{:,}".format(int(re.sub(",", "", data['rank']))), data['skillvalues'], data['questscomplete'], data['questsstarted'], data['questsnotstarted']]
