fba44bb79389a84b865c8f76d988ad47018ca89c
devmgardner <devm.gardner@gmail.com>
2021-10-31 15:04:28 +0000

    updates

--- ergosite/ergo/ergo/settings.py
+++ ergosite/ergo/ergo/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'feed.apps.FeedConfig'
+    'feed.apps.FeedConfig',
+    'django_tables2',
 ]
 
 MIDDLEWARE = [
