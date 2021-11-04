8bc605121ccb6442f6097dfc1b974cdc811c3819
devmgardner <devm.gardner@gmail.com>
2021-11-04 23:02:04 +0000

    toggle button added for progress bars

--- ergosite/ergo/feed/static/iportfolio/assets/js/dev.js
+++
@@ -1,8 +0,0 @@
-(function showhide() {
-    var x = document.getElementsByClassName("progress");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  })
