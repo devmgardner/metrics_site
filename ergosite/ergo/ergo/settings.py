50a8502f99cc004e5e4cd8362bdcceefa11ea75c
Dev M <57118036+devmgardner@users.noreply.github.com>
2021-12-21 14:23:01 -0500

    Debug mode turned off

--- ergosite/ergo/ergo/settings.py
+++ ergosite/ergo/ergo/settings.py
@@ -26,7 +26,7 @@ with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
