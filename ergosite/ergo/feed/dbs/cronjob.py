46779c12eed5b17f720907bb66fb9ceb1bbc1465
devmgardner <devm.gardner@gmail.com>
2021-12-21 11:14:21 -0500

    preparing to migrate

--- ergosite/ergo/feed/dbs/cronjob.py
+++ ergosite/ergo/feed/dbs/cronjob.py
@@ -1,7 +1,7 @@
 import git
-repo = git.Repo('path/to/git/repo')  # if repo is CWD just do '.'
+repo = git.Repo('../../../')  # if repo is CWD just do '.'
 
-repo.index.add([''])
-repo.index.commit('my commit description')
+repo.index.add(['.'])
+repo.index.commit('remote db updates')
 origin = repo.remote('origin')
 origin.push()
