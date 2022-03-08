af6a33e60d74bcc9ae86b171c54d1258632d978a
Dev M <57118036+devmgardner@users.noreply.github.com>
2022-03-08 18:57:31 -0500

    multiple url patterns

--- ergosite/ergo/feed/urls.py
+++ ergosite/ergo/feed/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('Ergo/', views.ergo, name='ergo'),
     path('Ergo', views.ergo, name='ergo'),
     path('alinthar/', views.alinthar, name='alinthar'),
+    path('alinthar', views.alinthar, name='alinthar'),
+    path('Alinthar/', views.alinthar, name='alinthar'),
+    path('Alinthar', views.alinthar, name='alinthar'),
     path('dasn1u/', views.dasn1u, name='dasn1u'),
+    path('dasn1u', views.dasn1u, name='dasn1u'),
+    path('Dasn1u/', views.dasn1u, name='dasn1u'),
+    path('Dasn1u', views.dasn1u, name='dasn1u'),
 ]
