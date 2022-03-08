d4044f9d5b6af7cd72b57610308c229802983c3d
Dev M <57118036+devmgardner@users.noreply.github.com>
2022-03-08 18:54:25 -0500

    case change test

--- ergosite/ergo/feed/urls.py
+++ ergosite/ergo/feed/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('ergo/', views.ergo, name='ergo'),
+    path('ergo', views.ergo, name='ergo'),
+    path('Ergo/', views.ergo, name='ergo'),
+    path('Ergo', views.ergo, name='ergo'),
     path('alinthar/', views.alinthar, name='alinthar'),
     path('dasn1u/', views.dasn1u, name='dasn1u'),
 ]
