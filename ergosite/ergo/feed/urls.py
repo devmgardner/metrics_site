adbb133dd869b6b9c1dad7f7c1f49e976a5af591
devmgardner <devm.gardner@gmail.com>
2022-03-10 19:43:36 +0000

    variable test

--- ergosite/ergo/feed/urls.py
+++ ergosite/ergo/feed/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ergo/', views.charname('','Ergo'), name='ergo'),
-    path('ergo', views.charname('','Ergo'), name='ergo'),
-    path('Ergo/', views.charname('','Ergo'), name='ergo'),
-    path('Ergo', views.charname('','Ergo'), name='ergo'),
-    path('alinthar/', views.charname('','Alinthar'), name='alinthar'),
-    path('alinthar', views.charname('','Alinthar'), name='alinthar'),
-    path('Alinthar/', views.charname('','Alinthar'), name='alinthar'),
-    path('Alinthar', views.charname('','Alinthar'), name='alinthar'),
-    path('dasn1u/', views.charname('','dasn1u'), name='dasn1u'),
-    path('dasn1u', views.charname('','dasn1u'), name='dasn1u'),
-    path('Dasn1u/', views.charname('','dasn1u'), name='dasn1u'),
-    path('Dasn1u', views.charname('','dasn1u'), name='dasn1u'),
+    path('ergo/', views.charname(player='Ergo'), name='ergo'),
+    path('ergo', views.charname(player='Ergo'), name='ergo'),
+    path('Ergo/', views.charname(player='Ergo'), name='ergo'),
+    path('Ergo', views.charname(player='Ergo'), name='ergo'),
+    path('alinthar/', views.charname(player='Alinthar'), name='alinthar'),
+    path('alinthar', views.charname(player='Alinthar'), name='alinthar'),
+    path('Alinthar/', views.charname(player='Alinthar'), name='alinthar'),
+    path('Alinthar', views.charname(player='Alinthar'), name='alinthar'),
+    path('dasn1u/', views.charname(player='dasn1u'), name='dasn1u'),
+    path('dasn1u', views.charname(player='dasn1u'), name='dasn1u'),
+    path('Dasn1u/', views.charname(player='dasn1u'), name='dasn1u'),
+    path('Dasn1u', views.charname(player='dasn1u'), name='dasn1u'),
 ]
